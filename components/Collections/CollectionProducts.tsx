dad817f66e37cd67ea84dfb9589aa649a30ce3c6
IT Alfa Scorpii <chrisdiantodoni@gmail.com>
2025-12-09 15:18:41 +0700

    update

--- components/Collections/CollectionProducts.tsx
+++ components/Collections/CollectionProducts.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import React from "react";
 import Link from "next/link";
@@ -58,7 +59,7 @@ const CollectionProducts = ({
 }: //   addToCartProduct,
 //   addToWishListProduct,
 {
-  products: unknown;
+  products: any[];
 }) => {
   const ClickHandler = () => {
     window.scrollTo(10, 0);
